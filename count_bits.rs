fcdcfbf39bc54522bdb183d24543ac4046191266
Vincent Foley <vfoley@gmail.com>
2017-01-11 09:22:26 -0500

    Ensure that count_bits follows C call conventions

--- count_bits.rs
+++ count_bits.rs
@@ -1,7 +1,7 @@
 #![crate_type = "staticlib"]
 
 #[no_mangle]
-pub fn count_bits(mut n: usize) -> usize {
+pub extern "C" fn count_bits(mut n: usize) -> usize {
     let mut count = 0;
     while n != 0 {
         count += n & 1;
